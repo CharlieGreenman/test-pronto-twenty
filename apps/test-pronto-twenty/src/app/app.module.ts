13adab09750614fb7b8c5320d89592b6302f26c2
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:45:11 -0500

    Add code for angular-16.2.0 integrate-footer.
    [Url: razroo.com/community/angular-16.2.0/footer/integrate-footer]

--- apps/test-pronto-twenty/src/app/app.module.ts
+++ apps/test-pronto-twenty/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderComponentModule } from "@test-pronto-twenty/common/ui";
+import { GlobalFooterModule } from "@test-pronto-twenty/common/ui";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
