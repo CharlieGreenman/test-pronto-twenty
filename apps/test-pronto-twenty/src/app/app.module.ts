578181fef25de6f8dc8a28915e3bddfa69b3ce3f
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:46:53 -0500

    Add code for angular-16.2.0 integrate-ngrx-store.
    [Url: razroo.com/community/angular-16.2.0/add-ngrx-store-infrastructure/integrate-ngrx-store]

--- apps/test-pronto-twenty/src/app/app.module.ts
+++ apps/test-pronto-twenty/src/app/app.module.ts
@@ -8,10 +8,17 @@ import { GlobalHeaderComponentModule } from "@test-pronto-twenty/common/ui";
 import { GlobalFooterModule } from "@test-pronto-twenty/common/ui";
 import { GlobalSidenavModule } from "@test-pronto-twenty/common/ui";
 import { MatListModule } from "@angular/material/list";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreModule } from "@ngrx/store";
+import { environment } from "@test-pronto-twenty/common/common-environment";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule, StoreModule.forRoot({}), StoreDevtoolsModule.instrument({
+    maxAge: 25, // Retains last 25 states
+    logOnly: environment.production, // Restrict extension to log-only mode
+    autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+  }),],
   providers: [],
   bootstrap: [AppComponent],
 })
