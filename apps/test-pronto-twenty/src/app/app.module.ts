0d5bd02f2e297524d6ab381a0fe81028de3934ff
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:45:22 -0500

    Add code for angular-16.2.0 integrate-side-nav.
    [Url: razroo.com/community/angular-16.2.0/side-nav-recipe/integrate-side-nav]

--- apps/test-pronto-twenty/src/app/app.module.ts
+++ apps/test-pronto-twenty/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderComponentModule } from "@test-pronto-twenty/common/ui";
 import { GlobalFooterModule } from "@test-pronto-twenty/common/ui";
+import { GlobalSidenavModule } from "@test-pronto-twenty/common/ui";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
