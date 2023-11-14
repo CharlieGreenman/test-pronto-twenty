bd0247c98568c0af1d18438dcae56e973af51274
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:44:20 -0500

    Add code for angular-16.2.0 add-browser-animations-module.
    [Url: razroo.com/community/angular-16.2.0/custom-material-design-recipe/add-browser-animations-module]

--- apps/test-pronto-twenty/src/app/app.module.ts
+++ apps/test-pronto-twenty/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
