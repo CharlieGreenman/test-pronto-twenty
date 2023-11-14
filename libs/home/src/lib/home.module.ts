5f5210b77348caf0f74253603e2f282c6027657a
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:44:57 -0500

    Add code for angular-16.2.0 integrate-homepage-routing.
    [Url: razroo.com/community/angular-16.2.0/add-homepage/integrate-homepage-routing]

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./home/home.component";
+import { HomeRoutingModule } from "./routing/home-routing.module";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeComponent],
+  declarations: [HomeComponent]
 })
-export class HomeModule {}
+export class HomeModule { }
