87868191c54ebe7c33f0f4b0faf9c918c9e2405f
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:44:52 -0500

    Add code for angular-16.2.0 integrate-app-routing.
    [Url: razroo.com/community/angular-16.2.0/add-homepage/integrate-app-routing]

--- apps/test-pronto-twenty/src/app/app.module.ts
+++ apps/test-pronto-twenty/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./routing/app-routing.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
